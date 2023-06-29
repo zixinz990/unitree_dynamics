4af2b2c31ea069e3b7d1c2f265585480c79232a0
Zixin Zhang <1186193932@qq.com>
2023-06-29 11:52:46 -0400

    Generate dynamics for each leg

--- load_go1_const.m
+++ load_go1_const.m
@@ -1,26 +1,42 @@
-%% UNITREE GO1 PHYSICAL PARAMETERS
-% joint 1 frame's origin is the same as the base frame
+function load_go1_const(leg_index)
+global hip_G thigh_G calf_G M_bh M_ht M_tc M_cf s_1 s_2 s_3
+% Load Unitree Go1 leg constants
+% FL, FR, RL, RR: 0, 1, 2, 3
+if (leg_index == 0)
+    x_sign = 1;
+    y_sign = 1;
+elseif (leg_index == 1)
+    x_sign = 1;
+    y_sign = -1;
+elseif (leg_index == 2)
+    x_sign = -1;
+    y_sign = 1;
+else
+    x_sign = -1;
+    y_sign = -1;
+end
 
+% The origin of joint 1 is the same as the base frame
 % Position of joint 2 in joint 1 frame
-thigh_offset_x = 0;
-thigh_offset_y = 0.0812;
+thigh_offset_x = x_sign * 0;
+thigh_offset_y = y_sign * 0.0812;
 thigh_offset_z = 0;
 
 % Position of joint 3 in joint 2 frame
-calf_offset_x = 0;
-calf_offset_y = 0;
+calf_offset_x = x_sign * 0;
+calf_offset_y = y_sign * 0;
 calf_offset_z = -0.213;
 
 % Position of foot in joint 3 frame
-foot_offset_x = 0;
-foot_offset_y = 0;
+foot_offset_x = x_sign * 0;
+foot_offset_y = y_sign * 0;
 foot_offset_z = -0.213;
 
 % Hip link physical properties
 hip_mass = 0.591;
 % COM position in joint 1 frame
-hip_com_x = -0.005657;
-hip_com_y = -0.008752;
+hip_com_x = x_sign * -0.005657;
+hip_com_y = y_sign * -0.008752;
 hip_com_z = -0.000102;
 % Spatial inertia matrix
 hip_G = [[0.000334008405, -0.000010826066, 0.000001290732;
@@ -32,7 +48,7 @@ hip_G = [[0.000334008405, -0.000010826066, 0.000001290732;
 thigh_mass = 0.92;
 % COM position in joint 2 frame
 thigh_com_x = -0.003342;
-thigh_com_y = -0.018054;
+thigh_com_y = y_sign * -0.018054;
 thigh_com_z = -0.033451;
 % Body inertia tensor
 thigh_G = [[0.004431760472, 0.000057496807, -0.000218457134;
@@ -81,3 +97,5 @@ s_2 = [w_2; -cross(w_2, p_2)];
 w_3 = [0; 1; 0];
 p_3 = p_2 + [calf_offset_x; calf_offset_y; calf_offset_z];
 s_3 = [w_3; -cross(w_3, p_3)];
+
+end
--- one_leg_dynamics.m
+++ one_leg_dynamics.m
@@ -1,7 +1,10 @@
-close all; clear; clc; format long;
+clc; close all; clear all; format long;
 
 addpath("ModernRobotics/packages/MATLAB/mr")
-go1_const;
+addpath("ModernRobotics/packages/MATLAB/mr/Symbolic")
+global hip_G thigh_G calf_G M_bh M_ht M_tc M_cf s_1 s_2 s_3
+
+load_go1_const(3); % FL, FR, RL, RR: 0, 1, 2, 3
 g = [0; 0; -9.81];
 Ftip = [0; 0; 0; 0; 0; 0];
 Mlist = cat(3, M_bh, M_ht, M_tc, M_cf);
@@ -9,9 +12,9 @@ Glist = cat(3, hip_G, thigh_G, calf_G);
 Slist = [s_1, s_2, s_3];
 
 %% NO SYMBOLIC TEST
-thetalist_ = [rad2deg(5); rad2deg(30); rad2deg(-60)];
-dthetalist_ = [0.3; 0.4; 0.5];
-ddthetalist_ = [0.2; 0.3; 0.4];
+thetalist_ = [0.1; 0.2; -0.4];
+dthetalist_ = [0.0; 0.0; 0.0];
+ddthetalist_ = [0.0; 0.0; 0.0];
 
 taulist = InverseDynamics(thetalist_, dthetalist_, ddthetalist_, g, Ftip, Mlist, Glist, Slist);
 disp(taulist);
@@ -27,7 +30,8 @@ taulist_ = double(subs(taulist_sym, [thetalist; dthetalist; ddthetalist], [theta
 disp(taulist_);
 
 %% GENERATE C++ FUNCTION
+% Change function name based on the index of leg
 matlabFunction(taulist_sym, ...
-               'file', 'GeneratedFunctions/go1_one_leg_inverse_dynamics.m', ...
+               'file', 'GeneratedFunctions/go1_RR_leg_inverse_dynamics.m', ...
                'vars', {[t1; t2; t3], [dt1; dt2; dt3], [ddt1; ddt2; ddt3]}, ...
                'outputs', {'taulist'});
